824c6a9f1bce9da51a3a9ce1caa18d985b7296ac
shmlv9 <denis2009shumiov@gmail.com>
2025-01-31 02:33:24 +0300

    Аccording to the PEP_8 rules

--- keyboards/login_keyboard.py
+++ keyboards/login_keyboard.py
@@ -1,10 +1,10 @@
 from aiogram.types import ReplyKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
+
 def login() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
     kb.button(text="Войти, как админ")
     kb.button(text="Войти, как пользователь")
     kb.adjust(2)
     return kb.as_markup(resize_keyboard=True)
-
--- keyboards/user_keyboards.py
+++ keyboards/user_keyboards.py
@@ -1,11 +1,12 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+
 def main() -> ReplyKeyboardMarkup:
     return ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="Выбрать точку")],
-        [KeyboardButton(text="Посмотреть график")],
-        [KeyboardButton(text="Связь с начальством")]
-    ],
-    resize_keyboard=True
-)
+        keyboard=[
+            [KeyboardButton(text="Выбрать точку")],
+            [KeyboardButton(text="Посмотреть график")],
+            [KeyboardButton(text="Связь с начальством")]
+        ],
+        resize_keyboard=True
+    )

ae2621b5d7277bc82b2515e7fcb72bf480cacdca
34elo <evsenik23@mail.ru>
2025-01-31 00:31:53 +0300

    вывод киосков кофе

--- keyboards/admin_keyboards.py
+++ keyboards/admin_keyboards.py
@@ -1,4 +1,4 @@
-from aiogram.types import InlineKeyboardButton, KeyboardButton
+from aiogram.types import InlineKeyboardButton, KeyboardButton, InlineKeyboardMarkup
 from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder, ReplyKeyboardMarkup
 
 
@@ -6,15 +6,19 @@ def main() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
     kb.button(text="Сформировать частичный график")
     kb.button(text="Отправить уведомления сотрудникам")
-    kb.button(text="Посмотреть график на точку")
+    kb.button(text="Графики на точках")
     kb.button(text="")
     kb.adjust(2)
     return kb.as_markup(resize_keyboard=True)
 
-def points_list(arg: list[str]) -> ReplyKeyboardMarkup:
-    kb = ReplyKeyboardBuilder()
-    for i in arg:
-        kb.button(text=i, callback_data=i)
-        print(i)
 
-    return kb.as_markup(resize_keyboard=True)
+def points_list(arg: list[str]) -> InlineKeyboardMarkup:
+    buttons = [
+        [],
+    ]
+    for item in arg:
+        buttons[0].append(InlineKeyboardButton(text=item, callback_data=item))
+        print(item)
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard

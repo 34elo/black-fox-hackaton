4c7cc18e9095ce85d5d89bb39ab986faf6c1bbec
34elo <evsenik23@mail.ru>
2025-01-30 22:39:04 +0300

    commit test

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -5,17 +5,22 @@ from keyboards.admin_keyboards import points_list
 
 router_admin_panel = Router()
 
-def auto_schedule() -> None: # сформировать график
+
+def auto_schedule() -> None:  # сформировать график
     pass
 
-def send_notification() -> None: # отправка уведомелний сотрудникам о работе
+
+def send_notification() -> None:  # отправка уведомелний сотрудникам о работе
     pass
 
-def get_point_info(arg) -> None: # Запрос в бд на получение инфы о точке
+
+def get_point_info(arg) -> None:  # Запрос в бд на получение инфы о точке
     pass
 
-def get_points() -> list[str]: # список таблиц с точками
-    return['Дзержика', 'Гагарина']
+
+def get_points() -> list[str]:  # список таблиц с точками
+    return ['Дзержика', 'Гагарина']
+
 
 @router_admin_panel.message(F.text == "Сформировать график")
 async def create_schedule(message: Message):
@@ -24,15 +29,18 @@ async def create_schedule(message: Message):
 
 
 @router_admin_panel.message(F.text == "Отправить уведомления сотрудникам")
-async def create_schedule(message: Message):
+async def send_notification_tg(message: Message):
     send_notification()
     await message.answer('Уведомления отправлены всем сотрудникам')
 
 
 @router_admin_panel.message(F.text == "Посмотреть график на точках")
-async def create_schedule(message: Message):
+async def check_points(message: Message):
     await message.answer('Выберите точку', reply_markup=points_list(get_points()))
 
-@router_admin_panel.callback_query(F.data)
-async def point_info(callback: CallbackQuery):
-    await callback.message.answer(callback.data)
+
+@router_admin_panel.callback_query(F.data == "Дзержика")
+async def callback_on(callback: CallbackQuery):
+    await callback.message.answer(callback.text)
+
+
--- keyboards/admin_keyboards.py
+++ keyboards/admin_keyboards.py
@@ -4,16 +4,17 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder, InlineKeyboardBuilder, 
 
 def main() -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
-    kb.button(text="Сформировать график")
+    kb.button(text="Сформировать частичный график")
     kb.button(text="Отправить уведомления сотрудникам")
-    kb.button(text="Посмотреть график на точках")
-    kb.button(text="Сформировать график")
+    kb.button(text="Посмотреть график на точку")
+    kb.button(text="")
     kb.adjust(2)
     return kb.as_markup(resize_keyboard=True)
 
 def points_list(arg: list[str]) -> ReplyKeyboardMarkup:
     kb = ReplyKeyboardBuilder()
     for i in arg:
-        kb.button(text=str(i), callback_data=i)
+        kb.button(text=i, callback_data=i)
+        print(i)
 
     return kb.as_markup(resize_keyboard=True)

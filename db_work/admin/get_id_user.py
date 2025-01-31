9a887c64f905e28f5114833864c6732e7545982b
34elo <evsenik23@mail.ru>
2025-01-31 14:05:17 +0300

    do

--- db_work/admin/get_id_user.py
+++ db_work/admin/get_id_user.py
@@ -1,8 +1,10 @@
 def get_id_user(name):
     import sqlite3
-    data = sqlite3.connect('../../data/users_data.sqlite')
-    data_cursor = data.cursor()
-    data_cursor.execute(f"select 'Username TG' from employees_wishes where 'ФИО' = {name}")
-    data = data_cursor.fetchone()
+    conn = sqlite3.connect('../../data/users_data.sqlite')
+    data_cursor = conn.cursor()
+    data = data_cursor.execute(f"select 'Username TG' from employees_wishes where 'ФИО' = '{name}'").fetchall()
 
     return data
+
+
+print(get_id_user('Петр Максимович Астафьев'))

c435f9a9c6c989ee254d9ded9df5c901ff6fbff7
34elo <evsenik23@mail.ru>
2025-02-01 22:11:38 +0300

    do

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -45,12 +45,12 @@ def get_points() -> list[str]:  # список таблиц с точками
 @router_admin_panel.message(F.text == "Сформировать частичный график")
 async def create_schedule(message: Message) -> None:
     auto_schedule_create('data/users_data.sqlite', 'data/schedule.sqlite')
-    await message.answer('График сформирован. Вы сможете ознакомиться с ним в меню "Графики на точках"')
+    await message.answer('График сформирован. Вы сможете ознакомиться с ним в меню "Расписание на точках"')
 
 
-@router_admin_panel.message(F.text == "Отправить уведомления сотрудникам")
+@router_admin_panel.message(F.text == "Отправить уведомление сотрудникам")
 async def send_notification_tg(message: Message, state: FSMContext) -> None:
-    await message.answer('Напишите ваше сообщение-рассылку')
+    await message.answer('Напишите ваше уведомление:')
     await state.set_state(NotificationText.text)
 
 
@@ -63,10 +63,8 @@ async def message_with_text(message: Message, state: FSMContext) -> None:
 
 @router_admin_panel.message(F.text == "Расписание на точках")
 async def check_points(message: Message) -> None:
-    text = 'Выберите точку из приведенных ниже\n\n'
-    for item in get_points():
-        text += ' - ' + item + '\n'
-    await message.answer(text, reply_markup=points_list(get_points()))
+    text = 'Выберите точку из приведенных ниже:\n\n'
+    await message.answer(text, reply_markup=points_list(get_points())       )
 
 
 @router_admin_panel.message(lambda message: message.text in get_points())
@@ -91,7 +89,7 @@ async def main_menu(message: Message) -> None:
 @router_admin_panel.callback_query(F.data == 'Редактировать')
 async def callback_query(callback: CallbackQuery, state: FSMContext) -> None:
     await callback.message.answer(
-        'Выберите день недели', keyboards = admin_keyboards.day_week())
+        'Выберите день недели', keyboards=admin_keyboards.day_week())
     await state.set_state(ScheduleText.smena)
 
 
@@ -111,7 +109,7 @@ async def smena(message: Message, state: FSMContext) -> None:
         res = days_of_week[m[0]]
         ScheduleText.smena = res
         await state.set_state(ScheduleText.worker)
-        await message.answer('Напишите полное ФИО работника, которого вы хотите назначить на смену')
+        await message.answer('Напишите ФИО сотрудника, которого вы хотите назначить на смену')
     except Exception as e:
         await message.answer('Ошибка, попробуйте снова')
 
@@ -122,7 +120,7 @@ async def worker(message: Message, state: FSMContext) -> None:
         ScheduleText.worker = message.text
         print(ScheduleText.worker, ScheduleText.smena, str(ScheduleText.points))
         insert_to_schedule(ScheduleText.points, ScheduleText.smena, ScheduleText.worker)
-        await message.answer('График сформирован. Ознакомится с ним вы можете нажав на кнопку "Расписание на точках"')
+        await message.answer('График сформирован. Ознакомиться с ним вы можете в меню "Расписание на точках"')
         await state.clear()
     except Exception as e:
         print(e)
@@ -131,7 +129,7 @@ async def worker(message: Message, state: FSMContext) -> None:
 
 @router_admin_panel.message(F.text == 'Связаться с сотрудником')
 async def contact(message: Message, state: FSMContext) -> None:
-    await message.answer('Введите ФИО контакт, которого вам нужен')
+    await message.answer('Введите ФИО сотрудника, с которым хотите связаться')
     await state.set_state(Contact.contact)
 
 
@@ -141,9 +139,9 @@ async def contact(message: Message, state: FSMContext) -> None:
     await state.clear()
 
 
-@router_admin_panel.message(F.text == 'Количество смен у работников')
+@router_admin_panel.message(F.text == 'Количество смен у сотрудников')
 async def top_users(message: Message, state: FSMContext) -> None:
-    text = 'Количество смен у работников\n\n'
+    text = 'Количество смен у сотрудников\n\n'
     datas = get_working_times()
     for item in datas:
         text += str(item[0]) + ' - ' + str(item[1]) + '\n'
